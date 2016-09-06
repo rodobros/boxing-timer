53dd96c8224b0b1a751d59f24e14f716d4f32495
Rodolphe Brossard <Rodobros@rodolphes-macbook-pro.local>
2016-09-06 14:01:44 +0800

    Initial Commit

---
+++ Boxing timer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Boxing timer
+//
+//  Created by Rodolphe Brossard on 2016-09-06.
+//  Copyright © 2016 Rodolphe Brossard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Boxing timer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Boxing timer
+//
+//  Created by Rodolphe Brossard on 2016-09-06.
+//  Copyright © 2016 Rodolphe Brossard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
